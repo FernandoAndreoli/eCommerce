da6bd7a6c7a8635d34a5cf1c052930d7b480cab3
FernandoAndreoli <81665327+FernandoAndreoli@users.noreply.github.com>
2025-06-13 15:47:37 -0300

    Update and rename Documentacion del Diseño de BD.txt to Script CRUD .sql
    act de archivo .sql con el SCRUD

---
+++ database/Script CRUD .sql
@@ -0,0 +1,24 @@
+-- SCRIPT DE CRUD DE USUARIOS
+✅ CREATE (Insertar nuevo usuario)
+
+INSERT INTO usuarios (nombre_usuario, contraseña, rol)
+VALUES ('juan23', 'clave123', 'usuario');
+
+✅ READ (Consultar usuarios)
+
+a. Todos los usuarios
+SELECT * FROM usuarios;
+
+b.  Un usuario específico
+SELECT * FROM usuarios
+WHERE nombre_usuario = 'juan23';
+
+✅ UPDATE (Modificar contraseña o rol)
+
+UPDATE usuarios
+SET contraseña = 'nuevaClave456', rol = 'admin'
+WHERE nombre_usuario = 'juan23';
+
+✅ DELETE (Eliminar usuario)
+DELETE FROM usuarios
+WHERE nombre_usuario = 'juan23';
